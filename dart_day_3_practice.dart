15a30b9e48d9b9e72a7135ddf2d2edb2a29f2504
Md.Arafat Islam Nazmul <rahmannazmul10@gmail.com>
2025-05-06 16:45:23 +0600

    Add files via upload

---
+++ dart_day_3_practice.dart
@@ -0,0 +1,48 @@
+void main() {
+  /*int num =0;
+  if(num>=0){
+    print("zero is positive");
+  }else if(num<0){
+    print("Zero is negitive");
+  }else{
+    print("zero is zero");
+  }*/
+  /* int num = 13;
+  if(num%2==0){
+    print("number is even");
+  }else if(num%2!=0){
+    print("number is odd");
+  }else{
+    print("kisui nah");
+  }*/
+  /*int age = 19;
+  dynamic gpa = 4.25;
+  int englishscore = 58;
+  if (age >= 18) {
+    if (gpa >= 4.00) {
+      if (englishscore >= 60) {
+        print("he is selected");
+      }
+    } else {
+      print("not selected");
+    }
+  }*/
+ /* int age=19;
+  dynamic gpa= 4.25;
+  int engscore= 58;
+  if(age>=18&&gpa>=4.00&&engscore>=60){
+    print("he is selected for admission");
+  }else{
+    print("he is not");
+  }*/
+  /*int unit=250;
+  int bill=0;
+  if(unit<=100){
+    bill= unit*5;
+  }else if(unit<=200){
+    bill=(100*5)+((unit-100)*7);
+  }else{
+    bill=(100*7)+(100*5)+((unit-200)*10);
+  }
+  print("total bill: $bill");
+}*/
